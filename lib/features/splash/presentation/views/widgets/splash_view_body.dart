057039dce15d4564ed61a45b7f44353f7ae43723
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-08-26 02:09:13 +0300

    Update splash view  and apply dark theme to app

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,15 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo,)],
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 6),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
     );
   }
 }
