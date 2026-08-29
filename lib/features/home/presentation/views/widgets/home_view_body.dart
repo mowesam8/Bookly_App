e2a4d9f791cc2407a892ea5c5c55259139e20797
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-08-29 17:15:59 +0300

    Add BestSellerListViewItem widget to HomeViewBody for displaying best seller books

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
@@ -14,10 +15,44 @@ class HomeViewBody extends StatelessWidget {
         const CustomAppBar(),
         const FeaturedBooksListView(),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 36),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
           child: Text("Best Seller", style: Styles.titleMeduim),
         ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24,),
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.7,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.book1),
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+      
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
