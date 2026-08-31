a5f6cbedf281ced49b694e021cde0d78119eedfa
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-08-31 21:38:11 +0300

    Add CustomBookImage widget and update BookDetailsViewBody and FeaturedBooksListView

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,9 +7,13 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 32),
-      child: Column(children: const [CustomBookDetailsAppBar()]),
+      child: Column(children: [const CustomBookDetailsAppBar(), Padding(
+        padding: EdgeInsets.symmetric(horizontal: width *.17, vertical: 27),
+        child: const CustomBookImage(),
+      )]),
     );
   }
 }
