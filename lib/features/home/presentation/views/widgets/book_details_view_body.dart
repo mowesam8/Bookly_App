e0cef4d9a36937411bbe1e0262d21a6faf3425ce
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-08-31 21:52:16 +0300

    Update BookDetailsViewBody with adding title and subtitle

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +12,28 @@ class BookDetailsViewBody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 32),
-      child: Column(children: [const CustomBookDetailsAppBar(), Padding(
-        padding: EdgeInsets.symmetric(horizontal: width *.17, vertical: 27),
-        child: const CustomBookImage(),
-      )]),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2, vertical: 27),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(height: 16),
+          Text('The Jungle Book', style: Styles.textStyle30),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
