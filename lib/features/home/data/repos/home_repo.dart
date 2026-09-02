8cb27d92f0c52ae3896b06c577e3aed31e8da230
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-09-03 00:12:18 +0300

    Add Failure class and update HomeRepo methods with return types

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
-abstract class HomeRepo{
-  featchBestSellerBooks();
-  featchFeaturedBooks();
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/books_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<BooksModel>>> featchBestSellerBooks();
+  Future<Either<Failure,List<BooksModel>>>  featchFeaturedBooks();
 }
