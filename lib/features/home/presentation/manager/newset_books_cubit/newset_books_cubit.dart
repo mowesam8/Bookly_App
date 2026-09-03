57f8f81b7427dd2fb0165a679bd18067e4ea0b0f
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-09-04 00:47:34 +0300

    finish implementation of Newset Books cubit

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/books_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit({required this.homeRepo}) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.featchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(errorMessage:failure.errorMessage ));
+    }, (books) {
+      emit(NewsetBooksSuccess(books: books));
+    },);
+  }
 }
