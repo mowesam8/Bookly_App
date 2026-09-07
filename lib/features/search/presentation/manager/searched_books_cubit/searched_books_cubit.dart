d6a3e891d7925a074a3a0baacf3ab07c4a87a3d8
Mohamed Wesam <mmohamedwesam111@gmail.com>
2026-09-07 23:36:30 +0300

    Implement  Searched Books method

--- lib/features/search/presentation/manager/searched_books_cubit/searched_books_cubit.dart
+++ lib/features/search/presentation/manager/searched_books_cubit/searched_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'searched_books_state.dart';
 
 class SearchedBooksCubit extends Cubit<SearchedBooksState> {
-  SearchedBooksCubit() : super(SearchedBooksInitial());
+  SearchedBooksCubit({required this.searchRepo}) : super(SearchedBooksInitial());
+  final SearchRepo searchRepo;
+
+  Future<void> fetchSearchedBooks() async {
+    emit(SearchedBooksLoading());
+    var result = await searchRepo.featchSearchedBooks(terms: '');
+    result.fold(
+      (failure) {
+        emit(SearchedBooksFailure(errorMessage: failure.errorMessage));
+      },
+      (books) {
+        emit(SearchedBooksSuccess(books: books));
+      },
+    );
+  }
 }
